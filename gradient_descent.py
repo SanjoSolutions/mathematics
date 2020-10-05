d215e68d9bb4b071c2cf0735ba8b1607ba33e896
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2020-10-05 10:42:49 +0200

    Makes epsilon configurable

--- gradient_descent.py
+++ gradient_descent.py
@@ -2,8 +2,8 @@ from gradient_walk import gradient_walk
 from no_op import no_op
 
 
-def gradient_descent(first_derivative, alpha=0.5, initial_value=0, max_steps=None, after_step=no_op):
-    return gradient_walk(first_derivative, update_x, alpha, initial_value, max_steps, after_step)
+def gradient_descent(first_derivative, alpha=0.5, epsilon=0.01, initial_value=0, max_steps=None, after_step=no_op):
+    return gradient_walk(first_derivative, update_x, alpha, epsilon, initial_value, max_steps, after_step)
 
 
 def update_x(x, alpha, slope):
--- gradient_walk.py
+++ gradient_walk.py
@@ -1,7 +1,7 @@
 from no_op import no_op
 
 
-def gradient_walk(first_derivative, update_x, alpha=0.5, initial_value=0, max_steps=None, after_step=no_op):
+def gradient_walk(first_derivative, update_x, alpha=0.5, epsilon=0.01, initial_value=0, max_steps=None, after_step=no_op):
     x = initial_value
     slope = first_derivative(x)
     step = 1
@@ -10,7 +10,6 @@ def gradient_walk(first_derivative, update_x, alpha=0.5, initial_value=0, max_st
     print('  slope', slope)
     print('  x', x)
 
-    epsilon = 0.01
     while (max_steps is None or step < max_steps) and abs(slope) > epsilon:
         x = update_x(x, alpha, slope)
         slope = first_derivative(x)
