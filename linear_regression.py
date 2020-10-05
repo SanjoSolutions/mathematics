8b74498094ed551b88d1141191c2dbbd37f05640
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2020-10-05 15:04:27 +0200

    Work towards supporting m and b in linear regression

--- linear_regression.py
+++ linear_regression.py
@@ -14,78 +14,96 @@ from gradient_descent import gradient_descent
 
 def linear_regression(points):
     variables = (
+        # m
         {
-            'first_derivative': lambda variables: calculate_first_derivative_total_distance(variables, points),
+            'first_derivative': lambda variables: calculate_m_first_derivative_total_distance(variables, points),
             'alpha': 0.00001,
             'initial_value': 1,
         },
+        # b
+        {
+            'first_derivative': lambda variables: calculate_b_first_derivative_total_distance(variables, points),
+            'alpha': 0.00001,
+            'initial_value': 0,
+        }
     )
     return gradient_descent(
         variables,
-        max_steps=132857,
+        max_steps=1000000,
         after_step=lambda variables: print('total distance', calculate_total_distance(variables, points))
     )
 
 
 def calculate_total_distance(variables, points):
-    slope = variables[0]['value']
     total_distance = 0
     for point in points:
-        total_distance += calculate_distance(slope, point)
+        total_distance += calculate_distance(variables, point)
     return total_distance
 
 
-def calculate_distance(slope, point):
-    point_x = point[0]
-    point_y = point[1]
-    # f(x) = mx
-    if slope == 0:
-        distance_to_point = abs(point_y)
+def calculate_distance(variables, point):
+    m = variables[0]['value']
+    b = variables[1]['value']
+    c = point[0]
+    d = point[1]
+    # f(x) = mx + b
+    if m == 0:
+        distance_to_point = abs(d - b)
     else:
-        # a = point_x
-        # b = point_y
-        # f2(x) = -1 / m * (x - a) + b
+        # c = point_x
+        # d = point_y
+        # f2(x) = -1 / m * (x - c) + d
 
         # f = f2
-        # <=> mx = -1 / m * (x - a) + b
-        # <=> x = (a + b m)/(m^2 + 1) and m^2 + 1!=0 and m!=0
-        # <=> x == (a + b m)/(1 + m^2) && 1 + m^2 != 0 && m != 0
-        point_on_line_x = (point_x + point_y * slope) / (slope ** 2 + 1)
-        point_on_line_y = slope * point_on_line_x  # f(x) = mx
-        distance_to_point = math.sqrt((point_x - point_on_line_x) ** 2 + (point_y - point_on_line_y) ** 2)
+        # <=> mx + b = -1 / m * (x - c) + d
+        # <=> x = (-b m + c + d m)/(m^2 + 1) and m^2 + 1!=0 and m!=0
+        # <=> x == (-b m + c + d m)/(1 + m^2) && 1 + m^2 != 0 && m != 0
+        point_on_line_x = (-b * m + c + d * m) / (m ** 2 + 1)
+        point_on_line_y = m * point_on_line_x + b  # f(x) = mx + b
+        distance_to_point = math.sqrt((c - point_on_line_x) ** 2 + (d - point_on_line_y) ** 2)
     return distance_to_point
-    # point_on_line_x = (1 / m * a + b) / (m + 1 / m)
-    # point_on_line_y = m * (1 / m * a + b) / (m + 1 / m)
-    # a = point_x
-    # b = point_y
-    # sqrt((a - (1 / m * a + b) / (m + 1 / m)) ** 2 + (b - m * (1 / m * a + b) / (m + 1 / m)) ** 2)
-    # <=> sqrt((b - a m)^2/(1 + m^2))
-    # f'(x) = -((a + b m) sgn(b - a m))/(m^2 + 1)^(3/2)
 
 
-def calculate_first_derivative_distance(slope, point):
-    point_x = point[0]
-    point_y = point[1]
-    a = point_x
-    b = point_y
-    m = slope
-    # point_on_line_x = (a + b * m) / (m ** 2 + 1)
-    # point_on_line_y = m * (a + b * m) / (m ** 2 + 1)
-    # distance_to_point = sqrt((a - (a + b * m) / (m ** 2 + 1)) ** 2 + (b - m * (a + b * m) / (m ** 2 + 1)) ** 2)
-    # Factor[Sqrt[(a - (a + b m)/(1 + m^2))^2 + (b - (m (a + b m))/(1 + m^2))^2]] = Sqrt[(b - a m)^2/(1 + m^2)]
-    # d/dm(distance_to_point) = (-(2 m (b - a m)^2)/(1 + m^2)^2 - (2 a (b - a m))/(1 + m^2))/(2 sqrt((b - a m)^2/(1 + m^2)))
-    denominator = (2 * math.sqrt((b - a * m) ** 2 / (1 + m ** 2)))
+def calculate_m_first_derivative_distance(variables, point):
+    m = variables[0]['value']
+    b = variables[1]['value']
+    c = point[0]
+    d = point[1]
+    # point_on_line_x = (-b * m + c + d * m) / (m ** 2 + 1)
+    # point_on_line_y = m * point_on_line_x + b
+    # distance_to_point = sqrt((c - (-b * m + c + d * m) / (m ** 2 + 1)) ** 2 + (d - m * (-b * m + c + d * m) / (m ** 2 + 1) + b) ** 2)
+    denominator = (2 * math.sqrt(((b + d) ** 2 - 2 * c * (b + d) * m + (4 * b ** 2 + c ** 2) * m ** 2) / (1 + m ** 2)))
     if denominator == 0:
         return 0
     else:
-        numerator = (-(2 * m * (b - a * m) ** 2) / (1 + m ** 2) ** 2 - (2 * a * (b - a * m)) / (1 + m ** 2))
+        numerator = ((-2 * c * (b + d) + 2 * (4 * b ** 2 + c ** 2) * m) / (1 + m ** 2) - (2 * m * ((b + d) ** 2 - 2 * c * (b + d) * m + (4 * b ** 2 + c ** 2) * m ** 2)) / (1 + m ** 2) ** 2)
         return numerator / denominator
 
 
-def calculate_first_derivative_total_distance(variables, points):
-    slope = variables[0]['value']
+def calculate_m_first_derivative_total_distance(variables, points):
     first_derivative_total_distance = 0
     for point in points:
-        first_derivative_distance = calculate_first_derivative_distance(slope, point)
+        first_derivative_distance = calculate_m_first_derivative_distance(variables, point)
+        first_derivative_total_distance += first_derivative_distance
+    return first_derivative_total_distance
+
+
+def calculate_b_first_derivative_distance(variables, point):
+    m = variables[0]['value']
+    b = variables[1]['value']
+    c = point[0]
+    d = point[1]
+    denominator = (2 * (1 + m ** 2) * math.sqrt(((b + d) ** 2 - 2 * c * (b + d) * m + (4 * b ** 2 + c ** 2) * m ** 2) / (1 + m ** 2)))
+    if denominator == 0:
+        return 0
+    else:
+        numerator = (2 * (b + d) - 2 * c * m + 8 * b * m ** 2)
+        return numerator / denominator
+
+
+def calculate_b_first_derivative_total_distance(variables, points):
+    first_derivative_total_distance = 0
+    for point in points:
+        first_derivative_distance = calculate_b_first_derivative_distance(variables, point)
         first_derivative_total_distance += first_derivative_distance
     return first_derivative_total_distance
--- linear_regression_example.py
+++ linear_regression_example.py
@@ -1,20 +1,19 @@
 from linear_regression import linear_regression, calculate_total_distance
 
-points = (
-    (1, 1),
-    (2, 2),
-    (3, 4)
-)
 # points = (
-#     (1, 1),
+#     (1, 2),
 # )
-
-print('total distance for m = 1', calculate_total_distance(({'value': 1},), points))
-print('total distance for m = 4 / 3', calculate_total_distance(({'value': 4 / 3},), points))
+points = (
+    (1, 2),
+    (2, 2),
+)
 
 result = linear_regression(points)
 print('result', result)
-slope = result[0]['value']
-print('slope result', slope)
+m = result[0]['value']
+b = result[1]['value']
+print('slope result', m)
+print('b result', b)
 
-assert abs(slope - 1.33) < 0.01
+assert m == 0
+assert b == 2
